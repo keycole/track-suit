6ecc5446056197d083e3181a4f6293c4962cd895
Nicole Graiff <nicole.graiff@gmail.com>
2020-09-07 22:31:59 -0400

    deploy

--- server.js
+++ server.js
@@ -10,7 +10,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useFindAndModify: false });
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 require("./routes/html-routes");
 
